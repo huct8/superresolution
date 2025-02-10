67f2591b875c202e6291c44f068b5103ed6e2f71
187J3X1-114514 <smjwoainni@qq.com>
2025-02-10 11:51:29 +0800

    修复未安装iris导致的崩溃

--- forge/src/main/java/io/homo/superresolution/forge/platform/IrisForgePlatform.java
+++ forge/src/main/java/io/homo/superresolution/forge/platform/IrisForgePlatform.java
@@ -1,11 +1,20 @@
 package io.homo.superresolution.forge.platform;
 
 import io.homo.superresolution.common.platform.IrisPlatform;
-import net.irisshaders.iris.api.v0.IrisApi;
+
+import java.lang.reflect.Method;
 
 public class IrisForgePlatform extends IrisPlatform {
     @Override
     public boolean isShaderPackInUse() {
-        return IrisApi.getInstance().isShaderPackInUse();
+        try{
+            Class<?> irisApiClazz = Class.forName("net.irisshaders.iris.api.v0.IrisApi");
+            Method getInstanceMethod = irisApiClazz.getMethod("getInstance");
+            Object irisApiInstance = getInstanceMethod.invoke(null);
+            Method isShaderPackInUseMethod = irisApiInstance.getClass().getMethod("isShaderPackInUse");
+            return (boolean) isShaderPackInUseMethod.invoke(irisApiInstance);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
